0a57add16a231845ec4daf8155fd8c1575583994
Nguyen Anh Tuan <nguyenanhtuandl1504@gmail.com>
2026-06-19 21:41:06 +0700

    fix: correct service registration

--- src/FlowFi.AnalyticsService/Program.cs
+++ src/FlowFi.AnalyticsService/Program.cs
@@ -1,12 +1,21 @@
 using FlowFi.AnalyticsService.Config;
+using FlowFi.AnalyticsService.Database;
 using FlowFi.Common.Middleware;
 using FlowFi.Common.OpenApi;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Addanalyticservice(builder.Configuration);
+builder.Services.AddAnalyticsService(builder.Configuration);
 
 var app = builder.Build();
+
+if (app.Configuration.GetValue<bool>("Database:MigrateOnStartup"))
+{
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<AnalyticsDbContext>();
+    await db.Database.MigrateAsync();
+}
 
 app.UseFlowFiErrorHandling();
 app.UseFlowFiCorrelationId();
